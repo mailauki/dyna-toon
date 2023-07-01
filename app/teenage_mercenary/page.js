e3d8d3f75f0287bd068cf728f24ae9c4f289b18a
Julie Evans <juliemevans17@gmail.com>
2023-07-01 14:37:17 -0500

    Update header

--- app/teenage_mercenary/page.js
+++ app/teenage_mercenary/page.js
@@ -1,5 +1,7 @@
+"use client"
 import Image from 'next/image'
 import data from '../api/data.json'
+import { AppBar, Toolbar, Typography } from '@mui/material'
 
 const chapters = [
   {
@@ -26,7 +28,7 @@ export default function Page() {
         priority
       />
 
-      <div className="z-20 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+      {/* <div className="z-20 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
@@ -40,7 +42,13 @@ export default function Page() {
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <p>By {data.author} & {data.artist}</p>
         </div>
-      </div>
+      </div> */}
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h6" sx={{ mr: 2, flexGrow: 1 }}>{data.title}</Typography>
+          <Typography>By {data.author} & {data.artist}</Typography>
+        </Toolbar>
+      </AppBar>
 
       {/* <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
         <Image
