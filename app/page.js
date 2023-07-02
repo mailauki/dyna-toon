959fa08f1bf6019f31c7a5ab521ad3f584fe521a
Julie Evans <juliemevans17@gmail.com>
2023-07-01 23:25:35 -0500

    Add scale transform to home

--- app/page.js
+++ app/page.js
@@ -2,14 +2,19 @@
 import Image from 'next/image'
 // import Link from 'next/link'
 import data from './api/data.json'
-import { ImageList, ImageListItem, ImageListItemBar, Link } from '@mui/material'
+import { ImageList, ImageListItem, ImageListItemBar, Link, Typography } from '@mui/material'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10">
       <ImageList>
         {data.map((series, index) => (
-          <ImageListItem component={Link} href={`/${series.slug}`} key={index}>
+          <ImageListItem 
+            component={Link} 
+            href={`/${series.slug}`} 
+            key={index}
+            sx={{ m: 2, transition: "transform 0.2s", "&:hover": { transform: "scale(1.05)"} }}
+          >
             <Image
               src={`/${series.slug}/square_landingpage.jpg`}
               alt={`${series.title} Thumbnail`}
@@ -18,9 +23,14 @@ export default function Home() {
               height={340}
             />
             <ImageListItemBar 
-              title={series.title}
+              title={<Typography variant="h5">{series.title}</Typography>}
               subtitle={`by: ${series.author} ${series.artist ? "& " + series.artist : ""}`}
-              position="below" 
+              // position="below"
+              // sx={{ background: "transparent" }}
+              sx={{
+                background: "linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)",
+                pt: 2
+              }}
             />
           </ImageListItem>
         ))}
